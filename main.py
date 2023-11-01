75487b8470a16b92feabdcff6f993e9b9e1890d8
MSimsek07 <masimsek.official@gmail.com>
2023-11-01 14:05:08 +0300

    base model preparation stage has been completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -7,9 +8,25 @@ STAGE_NAME = "Data Ingestion stage"
 
 try:
     logger.info(f">>>>>>>> Stage: {STAGE_NAME} started <<<<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-    logger.info(f">>>>>>>> Stage: {STAGE_NAME} completed <<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(
+        f">>>>>>>> Stage: {STAGE_NAME} completed <<<<<<<<\n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare base model"
+
+
+try:
+    logger.info(f"*************************")
+    logger.info(f">>>>>>>> Stage: {STAGE_NAME} started <<<<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(
+        f">>>>>>>> Stage: {STAGE_NAME} completed <<<<<<<<\n\nx============x")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -9,3 +9,16 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+
+# If frozen is true, fields may not be assigned to after instance creation.
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
